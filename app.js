cbfa022b68ecf9c19e32b86c9000e5113f27a0d7
Oscar Miras <oscar.miras@gmail.com>
2022-01-14 18:28:45 +0100

    Revert "solucion"
    This reverts commit 7224a604bfdce59c1d67b695230f1cf73ed0c9e8.

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-// solución JQuery
-
-$("#btn").click(function () {
-
-    let texto = $("#str").val();
-    $("#output").text(texto.length);
-
-    // Cambiar la fuente del resultado a 32px
-    $("#output").css({ "font-size": "30px", "border": "2px solid black" })
-});
+// Tu código aquí
