e9a02b25c7f8ece85ae02a56682165c04c3d1339
Alex <alexalonsoportas@gmail.com>
2022-01-16 20:35:09 +0100

    ejercicio hecho

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // Tu código aquí
+let btn = document.querySelector("#btn");
+let output = document.querySelector("#output");
+let texto = document.querySelector("#str");
+
+btn.addEventListener("click", escribirLongitud);
+
+function escribirLongitud(){
+    output.textContent = texto.value.length
+}
